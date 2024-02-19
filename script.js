236bc99c152969c808c7ffb411c0ba08913a13f2
Uhaha123 <160446263+Uhaha123@users.noreply.github.com>
2024-02-18 22:10:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ function myVideo() {
     var video = document.getElementById('myVideo');
     var button = document.getElementById('btn');
     var  stop = document.getElementById('stop');
-    video.autoplay = true;
-    video.loop= true;
+    
     if (video.paused) { 
         button.innerText = "Pause";
         button.disabled = true;
-        button.style.display="none";
         stop.style.display="block";
         video.loop= true;
         video.autoplay = true;
